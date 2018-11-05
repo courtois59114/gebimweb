8282da3bf63fdfbfce9c9cfe42f89db5fadc524d
Courtois <courtoiseric@gmail.com>
2018-11-05 18:37:07 +0100

    rename .php and add maps and form

--- site/index.php
+++ site/index.php
@@ -22,7 +22,7 @@
     <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/gebim.css" rel="stylesheet">
 
   </head>
 
@@ -224,11 +224,99 @@
       </div>
     </section>
 
-    <section class="bg-dark text-white">
-      <div class="container text-center">
-        <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-        <a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>
+
+    <section class="bg-dark text-white" id="contactform">
+    <?php
+    // FORMULAIRE DE CONTACT
+
+    if (isset($_POST["submit"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $to = 'contact@gebim.fr';
+        $subject = 'Message ou commande envoyé(e) depuis le site internet';
+        $body ="From: $name\n E-Mail: $email\n Téléphone: $phone\n Message:\n $message";
+        // Check if name has been entered
+        if (!$_POST['name'] || $_POST['name'] == "" ) {
+            $errName = 'S\'il vous plait, entrez votre nom';
+        }
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'S\'il vous plait, entrez une adresse email valide';
+        }
+        // Check if phone has been entered
+        if (!$_POST['phone'] || !filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT)) {
+            $errPhone = 'S\'il vous plait, entrez un numéro de téléphone valide';
+        }
+        //Check if message has been entered
+        if (!$_POST['message'] || !strlen(trim($_POST['message'])) ) {
+            $errMessage = 'S\'il vous plait, entrez votre message';
+        }
+        $headers .= 'From:' .utf8_decode($name). ' <' .$email.'>' . '\r\n' .
+            'Reply-To:'.$email. '\r\n' .
+            'Content-Type: text/plain; charset="utf-8"\r\n';
+        // If there are no errors, send the email
+        if (!$errName && !$errEmail && !$errPhone && !$errMessage) {
+            if ( mail($to, utf8_decode($subject), $body, $headers) ) {
+                $result = '<div class="alert alert-success">Merci, nous vous recontacterons au plus vite!</div>';
+            } else {
+                $result = '<div class="alert alert-danger">Une erreur est survenue lors de l\'envoi. Veuillez réessayer plus tard ou nous contacter par téléphone. Merci!</div>';
+            }
+        }
+    }
+?>
+
+
+      <div class="container">
+          <div class="row">
+              <div class="box">
+                  <div class="col-lg-12">
+                      
+                          <h2 class="intro-text text-center"><strong>Nous Contacter</strong></h2>
+                      <hr>
+                      <p class="text-justify">Professionnel ou particulier, v.............ns notre savoir-faire à votre service: nous livrons quotidiennement dans la métropole lilloise, avec une équipe et des installations en conséquence. 
+                      </p>
+                      <p class="text-justify">Mariages, conférences, occasions particulières, vous voulez agrémenter vos évènements : nous sommes là pour vos petits-déjeuners, brunchs, buffets et autres cocktails, envoyez-nous votre commande en ligne, nous vous répondrons dans les plus brefs délais! 
+                      </p>
+                      <form role="form" method="post" action="index.php#contactform" >
+                          <div class="row">
+                              <div class="form-group col-lg-4">
+                                  <label for="name">Votre nom</label>
+                                  <input id="name" name="name" type="text" class="form-control" value="<?php if (isset($_POST['name'])) { echo htmlspecialchars($_POST['name']); } ?>">
+                                  <?php echo "<p class='text-danger'>$errName</p>";?>
+                              </div>
+                              <div class="form-group col-lg-4">
+                                  <label for="email">Votre email</label>
+                                  <input id="email" name="email" type="email" class="form-control" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                                  <?php echo "<p class='text-danger'>$errEmail</p>";?>
+                              </div>
+                              <div class="form-group col-lg-4">
+                                  <label for="phone">Votre téléphone</label>
+                                  <input id="phone" name="phone" type="tel" class="form-control" value="<?php echo htmlspecialchars($_POST['phone']); ?>">
+                                  <?php echo "<p class='text-danger'>$errPhone</p>";?>
+                              </div>
+                              <div class="clearfix"></div>
+                              <div class="form-group col-lg-12">
+                                  <label for="message">Votre demande</label>
+                                  <textarea placeholder="Détaillez votre demande" name="message" class="form-control" rows="6">
+                                      <?php echo htmlspecialchars($_POST['message']); ?>
+                                  </textarea>
+                                  <?php echo "<p class='text-danger'>$errMessage</p>";?>
+                              </div>
+                              <div class="form-group col-lg-12">
+                                  <input type="hidden" name="save">
+                                  <button id="submit" name="submit" type="submit" value="Send" class="btn btn-default">Envoyer</button>
+                              </div>
+                          </div>
+                      </form>
+                      <?php echo $result; ?>
+                  </div>
+              </div>
+          </div>
       </div>
+    
+
     </section>
 
     <section id="contact">
@@ -254,6 +342,69 @@
         </div>
       </div>
     </section>
+
+    <section id="contact2"> 
+      <div class="container">
+        <div class="row">
+          <!-- <div class="box"> -->
+                <div class="col-md-8 large-padding">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1265.1941586866335!2d3.069908558336965!3d50.638478994875435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d58a72f2db17%3A0xf2d9702ba07dbc04!2s12+Place+Saint-Hubert%2C+59800+Lille!5e0!3m2!1sfr!2sfr!4v1541437580437" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen scrolling="no" marginheight="0" marginwidth="0" ></iframe>                         
+                </div>
+                <div class="col-md-4 text-center large-padding" >
+                    <h2 class="intro-text">
+                        <strong>Horaires</strong>
+                    </h2>     
+                    <table class="table">
+                        <tbody>
+                          <tr>
+                            <td>Lundi</td>
+                            <td>7h - 15h</td>
+                          </tr>
+                          <tr>
+                            <td>Mardi</td>
+                            <td>7h - 15h</td>
+                          </tr>
+                          <tr>
+                            <td>Mercredi</td>
+                            <td>7h - 15h</td>
+                          </tr>
+                          <tr>                            
+                            <td>Jeudi</td>
+                            <td>7h - 15h</td>
+                          </tr>
+                          <tr>                            
+                            <td>Vendredi</td>
+                            <td>7h - 15h</td>
+                          </tr>
+                          <tr>                            
+                            <td>Samedi</td>
+                            <td>Fermé</td>
+                          </tr>
+                          <tr>                            
+                            <td>Dimanche</td>
+                            <td>Fermé</td>                            
+                          </tr>
+                        </tbody>
+                    </table>
+                    <h2 class="intro-text">
+                        <strong>Coordonnées</strong>
+                    </h2>
+                    <table class="table big-text">
+                      <tbody>
+                        <tr>
+                          <td>22, rue de Roubaix, Lille
+                            <br>03.20.31.32.73
+                            <br><a href="mailto:artdupain@orange.fr">artdupain@orange.fr</a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                </div>
+                <div class="clearfix"></div>
+            <!--</div>-->
+         </div> 
+      </div>
+    </section>    
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
